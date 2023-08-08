d4754cfd69f22925e6d2474ccb7e26d9a7717e57
Thiago Mattos <thiagomattos.07@hotmail.com>
2023-08-08 03:34:56 -0300

    Manutenção no código

--- src/conta_bancaria.py
+++ src/conta_bancaria.py
@@ -14,36 +14,40 @@ numero_saques = 0
 
 while True:
     opcao = input(menu).lower()
-    if opcao == 'd':
+    if opcao == 'd': #Função que vai definir o deposito
         valor = float(input("Digite o valor do deposito: "))
         if valor > 0:
             saldo += valor
             extrato += f'Deposito R$:{valor:.2f}\n'
-            print(f"Valor adicionado! seu saldo é {saldo}")
+            print(f"VALOR ADICIONADO! SEU NOVO SALDO É: {saldo}")
 
-    elif opcao == "s":
-        valor = float(input("Digite o valor do saque: "))
+    elif opcao == "s": #Função que vai definir o saque
+        try:
+           valor = float(input("Digite o valor do saque: "))
         
-        limite_diario =  numero_saques >= LIMITE_SAQUE
+           limite_diario =  numero_saques >= LIMITE_SAQUE
        
-        saque_limite = valor > limite_saque
+           saque_limite = valor > limite_saque
 
-        valor_insuficiente = valor > saldo
+           valor_insuficiente = valor > saldo
         
-        if saque_limite:
+           if saque_limite:
             print("LIMITE DE SAQUES ATINGIDO")
-        elif limite_diario:
+           elif limite_diario:
             print("LIMITE DE SAQUES DIÁRIOS ATINGIDO")
-        elif valor_insuficiente:
+           elif valor_insuficiente:
             print("VALOR INSUFICIENTE")
         
-        elif valor > 0:
-            saldo -= valor
-            extrato += f'Deposito R${valor:.2f}\n'
-            numero_saques +=1
-            print(f"VALOR SACADO SEU SALDO É {saldo}")
-
-    elif opcao == "e":
+           elif valor > 0:
+             saldo -= valor
+             extrato += f'Saque R${valor:.2f}\n'
+             numero_saques +=1
+             print(f"VALOR SACADO SEU NOVO SALDO É {saldo}")
+        except ValueError:
+           print("Entrada inválida. Digite um valor numérico válido.")
+           
+ 
+    elif opcao == "e": #função para exibir o extrato
         print("=========EXTRATO=========")
         print(f'seu extrato {extrato:.2f}\n' if not extrato else extrato)
         print(f"Seu Saldo atual {saldo:.2f}\n")
